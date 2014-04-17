aea286bcda55793e4d13bbcac139a0cf20245adf
Devin Schwab <digidevin@gmail.com>
2014-04-17 19:57:01 -0400

    Added lines to do the message packing

--- scripts/HDI_state.py
+++ scripts/HDI_state.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 import rospy
 from software_driving_interface.msg import HDI_feedback
+import struct
 
 
 def callback(msg):
    rospy.loginfo(rospy.get_name() + " received the following: wa: %f wf: %f v: %f" % (msg.wheel_angle, msg.wheel_force, msg.vibration))
+   packed_msg = struct.pack('<ddd', msg.wheel_angle, msg.wheel_force, msg.vibration)
+   rospy.loginfo(rospy.get_name() + " packed message into %s" % packed_msg.encode('hex'))
 
 
 
